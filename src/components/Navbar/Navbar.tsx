414eaf0d8ac3d84004a6752f3e490bacbc6b0a1f
Fatih İnci <fatih969692@gmail.com>
2023-01-23 08:18:15 +0300

    first commit

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,11 @@
 import { useSelectedLayoutSegment } from "next/navigation";
 import React from "react";
 import { Navbars } from ".";
+{/*
+What is the Client Components?
+Client Components enable you to add client-side interactivity to your application. In Next.js, they are prerendered on the server and hydrated on the client. 
 
+*/}
 export default function Navbar() {
   const segment = useSelectedLayoutSegment() ?? "default";
   const SegmentNavbar = Navbars[segment as keyof typeof Navbars];
